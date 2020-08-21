77b4883f1e074bdbe1f0a60c139820c626d0790e
Angel Rodriguez <venegasrv@outlook.com>
2020-08-21 16:04:38 -0500

    Documentacion de la Clase Game

--- Game.java
+++ Game.java
@@ -8,49 +8,56 @@ import java.util.Queue;
  * @version 1.0
  */
 public class Game {
-    
-    public static void main(String[] args) {
-        View view = new View();
-        Player currentPlayer;
-        Table table = new Table();
-        Queue<Player> queue = new LinkedList<Player>();
-
-        queue.add(new Player("PLAYER 1",'x', 0));
-        queue.add(new Player("PLAYER 2",'o', 1));
-        currentPlayer = queue.peek();
-
-        int x = 0;
-        int y = 0;
-
-        view.setVisible(true);
-
-        while (!table.gameOver()) {
-            if (!table.isEmptyTable()) {
-              try {
-                currentPlayer = queue.peek();
-                x = view.getPositionX();
-                y = view.getPositionY();
-                if (!table.isEmptyBox(x,y)){
-                  table.add(currentPlayer.getSymbol(),x,y);
-                  view.setCurrentImage(currentPlayer.getNumber());
-                  System.out.println(currentPlayer.getName());
-                  System.out.println(table.print());
-                  queue.add(queue.poll());
-                }
-                else
-                  throw new Exception();
-              }
-              catch (Exception e) {
-              }
-            }
-            else break;
+  /**
+   * Funcion principal del juego
+   * @param args
+   */
+  public static void main(String[] args) {
+    //Instanceamos una View
+    View view = new View();
+    //Instanceamos un Player
+    Player currentPlayer;
+    //Insatanceamos una tabla
+    Table table = new Table();
+    //Instanceamos un Queue de Player
+    Queue<Player> queue = new LinkedList<Player>();
+    //Agregamos los jugadores al Queue
+    queue.add(new Player("PLAYER 1",'x', 0));
+    queue.add(new Player("PLAYER 2",'o', 1));
+    currentPlayer = queue.peek();
+    //X y Y capturaran las cordenadas del View
+    int x = 0;
+    int y = 0;
+    //HAcemos visible al View
+    view.setVisible(true);
+    //Bucle principal
+    while (!table.gameOver()) {
+      if (!table.isEmptyTable()) {
+        try {
+          currentPlayer = queue.peek();
+          x = view.getPositionX();
+          y = view.getPositionY();
+          if (!table.isEmptyBox(x,y)){
+            table.add(currentPlayer.getSymbol(),x,y);
+            view.setCurrentImage(currentPlayer.getNumber());
+            System.out.println(currentPlayer.getName());
+            System.out.println(table.print());
+            queue.add(queue.poll());
+          }
+          else
+            throw new Exception();
         }
-
-        if (table.gameOver())
-            System.out.print("El ganador es:" + currentPlayer.getName());
-        else
-            System.out.print("Empate");
-
+        catch (Exception e) {
+        }
+      }
+      else break;
     }
+    //Verificar quien gano
+    if (table.gameOver())
+      System.out.print("El ganador es:" + currentPlayer.getName());
+    else
+      System.out.print("Empate");
+
+  }
 
 }
